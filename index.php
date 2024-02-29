7244ca428c005b3d39120d74cd8f89fe829e7914
Xatenev <Xatenev@users.noreply.github.com>
2024-02-29 19:44:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
+
 namespace Test;
+
 require_once 'autoloader.php';
 
-
-// instantiate the loader
+// Instantiate the loader
 $loader = new Psr4AutoloaderClass();
 
-// register the autoloader
+// Register the autoloader
 $loader->register();
-// register the base directories for the namespace prefix
-$loader->addNamespace('Test\Src', realpath(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
+// Register the base directories for the namespace prefix
+$loader->addNamespace('Test\Src', realpath(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
 $example = new \Test\Src\Example();
 
